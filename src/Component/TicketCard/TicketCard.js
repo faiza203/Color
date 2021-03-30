01e3dc001e80d09b565b963e7f12a5eb2d8dfaac
faiza203 <fiazfaiza981@gmail.com>
2021-03-30 17:45:17 +0500

    Add  function to hidden and visible

--- src/Component/TicketCard/TicketCard.js
+++ src/Component/TicketCard/TicketCard.js
@@ -4,53 +4,92 @@ import "./TicketCard.css";
 
 import {
   Card,
-  CardColumns,
   CardText,
   CardBody,
   CardTitle,
   CardSubtitle,
-  CardHeader,
   CardFooter,
-  Button,
-  TabContent,
-  TabPane,
-  Nav,
-  NavItem,
-  NavLink,
+  CardLink,
 } from "reactstrap";
 
 import { Link, BrowserRouter as Router } from "react-router-dom";
 
 class TicketCard extends React.Component {
+  changeState = () => {
+    const card = document.querySelector(".card"); 
+    card.classList.forEach((classname) =>{
+      console.log(classname);
+    classname === "false" ? card.classList.remove("false") : card.classList.add("false")
+    });
+    console.log(card.classList);
+  };
+
   render() {
     return (
       <div className="mb-2 mt-2 ticketCardWidth">
-        <div>
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">
-                Action
-              </a>
-              <a class="dropdown-item" href="#">
-                Another action
-              </a>
-              <a class="dropdown-item" href="#">
-                Something else here
-              </a>
+        <div className="mb-2 actions">
+          <div className=" d-inline-flex drpdwn">
+            <p className="m-b-0">Action</p>
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              ></button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <i className="far fa-eye"></i> View
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <i className="far fa-trash-alt"></i> Delete
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <i className="far fa-comments"></i> Comment
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <i className="far fa-file-pdf"></i> Download
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <i className="fas fa-print"></i> Print
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <i className="fas fa-archive"></i> Archive
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    <span
+                      className="iconify"
+                      data-icon="ion-md-share"
+                      data-inline="false"
+                    ></span>{" "}
+                    Share
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
+          <button className="min" onClick={() => this.changeState()}>
+            <i className="fa fa-minus"></i>
+          </button>
         </div>
-        <Card className="border-0 ">
+        <Card className="border-0 m-t-0 card ">
           <CardBody>
             <CardTitle tag="h4" className="title m-t-0">
               Ticket title
@@ -142,10 +181,10 @@ class TicketCard extends React.Component {
                   className="btn btn-purple btn-xs m-r-5 m-b-5"
                 >
                   <span
-                    class="iconify"
+                    className="iconify"
                     data-icon="ion-md-share"
                     data-inline="false"
-                  ></span>{" "}
+                  ></span>
                 </button>
               </Link>
               <Link to="/bootstrap-4" className="card-link" title="save as PDF">
